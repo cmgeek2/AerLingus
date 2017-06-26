4f01900bf83532978060b92305b5a2e94c48e79d
Luis Coto <lcoto@everesttech.com>
2017-06-26 13:30:01 -0400

    common.js file has been added. It contains some javascript functions needed to parse xml files and populate drop downs.

---
+++ js/common.js
@@ -0,0 +1,103 @@
+﻿function setSelect(selectid, xmlpath, xmlnode, firstOption) {
+    var loadingtext = '-- Loading --';
+    var loadinghtml = '<option value="">' + loadingtext + '</option>';
+    var randomno = Math.ceil(Math.random() * 999);
+
+    $(selectid).html(loadinghtml);
+
+    $.ajax({
+        url: xmlpath + '?=rn' + randomno,
+        success: function (xml) {
+            parseSelectXML(xml, selectid, xmlnode, firstOption);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            alert(xhr.status);
+            alert(thrownError);
+        }
+    });
+}
+
+
+function parseSelectXML(xml, selectid, xmlnode, firstOption) {
+    var firstoption = firstOption;
+    var firsthtml = '<option value="">' + firstoption + '</option>';
+    var selecthtml = '';
+
+    $(xml).find(xmlnode).each(function () {
+
+        var selecttext = $(this).find('ID').attr('value');
+        var selectvalue = $(this).find('Name').attr('value');
+        selecthtml += '<option value="' + selectvalue + '">' + selecttext + '</option>';
+    });
+
+    $(selectid).html(firsthtml + selecthtml);
+}
+
+
+
+
+function IsEmail(email) {
+    var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if (!regex.test(email)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function setSelectQuery(selectid, xmlpath, xmlnode) {
+    var loadingtext = '-- Loading --';
+    var loadinghtml = '<option value="">' + loadingtext + '</option>';
+    var randomno = Math.ceil(Math.random() * 999);
+
+    $(selectid).html(loadinghtml);
+
+    $.ajax({
+        url: xmlpath + '?=rn' + randomno,
+        success: function (xml) {
+            parseSelectXML1(xml, selectid, xmlnode);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            alert(xhr.status);
+            alert(thrownError);
+        }
+    });
+}
+
+function parseSelectXML1(xml, selectid, xmlnode) {
+    var firstoption = '-- Please Select --';
+    var firsthtml = '<option value="">' + firstoption + '</option>';
+    var selecthtml = '';
+
+    $(xml).find(xmlnode).each(function () {
+
+        var selecttext = $(this).find('ID').attr('value');
+        var selectvalue = $(this).find('RC').attr('value');
+        selecthtml += '<option value="' + selectvalue + '{' + selecttext + '">' + selecttext + '</option>';
+    });
+
+    $(selectid).html(firsthtml + selecthtml);
+}
+
+function checkvalue(value) {
+    var inputelement = document.getElementById(value.id);
+    if (value.id == "email") {
+        var email = document.getElementById("_helpQueryEmail");
+
+        if (inputelement.value != email.value) {
+
+            alert("Emails do not match");
+            $("#Submitbtn").attr("disabled", true);
+
+        } else {
+            $("#Submitbtn").attr("disabled", false);
+        }
+    }
+
+    if (inputelement.value != "") {
+        inputelement.classList.add("filled");
+    } else if (inputelement.value == "") {
+        inputelement.classList.remove("filled");
+    }
+
+}
