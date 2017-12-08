2b54846e2f2eaff1074fda1ad9ab1f63dd76244f
cmgeek2 <ptmyres@gmail.com>
2017-12-07 22:11:36 -0500

    flight related js

--- RefundRequest.aspx
+++ RefundRequest.aspx
@@ -811,7 +811,7 @@
          setSelect('#salutationGuest4', 'Titles.xml', 'title', 'Title');
          setSelect('#salutationGuest5', 'Titles.xml', 'title', 'Title');
          setSelect('#salutationGuest6', 'Titles.xml', 'title', 'Title');
-         setSelect('#refundReasonDropDownList1', 'RefundReasons.xml', 'reason', 'Please Select');
+         setSelect('#refundReasonDropDownList1', 'RefundReasons.xml', 'reason', 'Please Select', 'Flight Related');
          setSelect('#refundReasonDropDownList2', 'RefundReasons.xml', 'reason', 'Please Select');
          setSelect('#refundReasonDropDownList3', 'RefundReasons.xml', 'reason', 'Please Select');
          setSelect('#refundReasonDropDownList4', 'RefundReasons.xml', 'reason', 'Please Select');
---
+++ common.js
@@ -0,0 +1,109 @@
+﻿function setSelect(selectid, xmlpath, xmlnode, firstOption, defaultselect) {
+    var loadingtext = '-- Loading --';
+    var loadinghtml = '<option value="">' + loadingtext + '</option>';
+    var randomno = Math.ceil(Math.random() * 999);
+
+    $(selectid).html(loadinghtml);
+
+    $.ajax({
+        url: xmlpath + '?=rn' + randomno,
+        success: function (xml) {
+            parseSelectXML(xml, selectid, xmlnode, firstOption, defaultselect);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            alert(xhr.status);
+            alert(thrownError);
+        }
+    });
+}
+
+
+function parseSelectXML(xml, selectid, xmlnode, firstOption,defaultselect) {
+    var firstoption = firstOption;
+    var firsthtml = '<option value="">' + firstoption + '</option>';
+    var selecthtml = '';
+
+    $(xml).find(xmlnode).each(function () {
+
+        var selecttext = $(this).find('ID').attr('value');
+        var selectvalue = $(this).find('Name').attr('value');
+        if (selectvalue == defaultselect){ 
+            selecthtml += '<option value="' + selectvalue + '" selected>' + selecttext + '</option>';
+        }
+        else
+        {
+            selecthtml += '<option value="' + selectvalue + '">' + selecttext + '</option>';
+        }
+    });
+
+    $(selectid).html(firsthtml + selecthtml);
+}
+
+
+
+
+function IsEmail(email) {
+    var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if (!regex.test(email)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function setSelectQuery(selectid, xmlpath, xmlnode) {
+    var loadingtext = '-- Loading --';
+    var loadinghtml = '<option value="">' + loadingtext + '</option>';
+    var randomno = Math.ceil(Math.random() * 999);
+
+    $(selectid).html(loadinghtml);
+
+    $.ajax({
+        url: xmlpath + '?=rn' + randomno,
+        success: function (xml) {
+            parseSelectXML1(xml, selectid, xmlnode);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            alert(xhr.status);
+            alert(thrownError);
+        }
+    });
+}
+
+function parseSelectXML1(xml, selectid, xmlnode) {
+    var firstoption = '-- Please Select --';
+    var firsthtml = '<option value="">' + firstoption + '</option>';
+    var selecthtml = '';
+
+    $(xml).find(xmlnode).each(function () {
+
+        var selecttext = $(this).find('ID').attr('value');
+        var selectvalue = $(this).find('RC').attr('value');
+        selecthtml += '<option value="' + selectvalue + '{' + selecttext + '">' + selecttext + '</option>';
+    });
+
+    $(selectid).html(firsthtml + selecthtml);
+}
+
+function checkvalue(value) {
+    var inputelement = document.getElementById(value.id);
+    if (value.id == "email") {
+        var email = document.getElementById("_helpQueryEmail");
+
+        if (inputelement.value != email.value) {
+
+            alert("Emails do not match");
+            $("#Submitbtn").attr("disabled", true);
+
+        } else {
+            $("#Submitbtn").attr("disabled", false);
+        }
+    }
+
+    if (inputelement.value != "") {
+        inputelement.classList.add("filled");
+    } else if (inputelement.value == "") {
+        inputelement.classList.remove("filled");
+    }
+
+}
